6db2a8d54b0c73626fb6c32159746e4a07680a11
Rohit Suthar <rohitsutharrr@gmail.com>
2025-08-05 18:47:50 +0530

    Add files via upload

---
+++ Snake-Water-Gun game/main.py
@@ -0,0 +1,40 @@
+"""
+1 for the snake
+-1 for the water
+0 for the gun
+"""
+computer = -1
+you = input("Enter your Choice :")
+youDict = {'s':1,'w':-1,'g':0}
+youNum = youDict[you]
+
+#check the condition for the cheaking the user imput and computer input
+
+if(computer == youNum):
+    print("it's Draw !")
+else:
+    if(computer == -1 and youNum == 0):
+        print("You Lose !")
+
+    elif(computer == 1 and youNum == 0):
+        print("You Win !")
+
+    elif(computer == -1 and youNum == 1):
+        print("You Win !")
+
+    elif(computer == 1 and youNum == -1):
+        print("You lose !")
+
+    elif(computer == 1 and youNum == 0):
+        print("You Win !")
+
+    elif(computer == -1 and youNum == 0):
+        print("You lose !")
+
+    elif(computer == 0 and youNum == 1):
+        print("You lose !")
+
+    elif(computer == 0 and youNum == -1):
+        print("You Win !")
+
+    else:print("Smething went wrong !")
